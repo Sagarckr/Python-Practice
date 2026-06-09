9f6e738ff528de4bdc2ae03b83c1ca7ec0943da6
Sagarckr <sagarsharma9821315721@gmail.com>
2026-06-09 07:00:35 +0545

    description added

--- day5.py
+++ day5.py
@@ -1,1 +1,20 @@
 # Github practice ...
+# install git, create SSH key using ssh-keygen, copy key to github
+# git init
+# git status
+# git add .
+# git commit -m "commit msg"
+# git remove -v:
+# origin  git@github.com:username/repo.git (fetch)
+# origin  git@github.com:username/repo.git (push)
+# git remote add origin <remote-url>
+# git remote add origin <remote-url>
+# git branch
+# git branch -M main
+# git push origin main
+# git branch new_branch
+# git checkout -b new_branch
+# git branch
+# git checkout new_branch
+# git pull
+# git clone <remote-url>
