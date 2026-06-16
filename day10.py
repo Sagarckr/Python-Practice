811f72d0a4233d0858750cd2207b0cc4f6c68524
Sagarckr <sagarsharma9821315721@gmail.com>
2026-06-16 07:57:49 +0545

    day9 function added

---
+++ day10.py
@@ -0,0 +1,57 @@
+# tuple and set
+
+# data = (1, 2, 3, "apple", True)
+# print(data)
+
+# print(data[0])
+# print(data[3])
+# print(data[1:4])
+# print(type(data))
+
+# for item in (123, "sagar", "sharma"):
+#     print(item)
+
+# my_list = [1, 2, 3]
+# print(type(my_list))
+# my_tuple = tuple(my_list)
+
+# my_tuple = (1, 2, 3)
+# my_list = list(my_tuple)
+# print(my_list)
+
+# t1 = (1, 2)
+# t2 = (3, 4)
+# print(t1 + t2)   # (1, 2, 3, 4)
+
+# t1 = ("sagar",)
+# print(t1 * 3)  # ('sagar', 'sagar', 'sagar')
+
+# a ={} # empty dictionary
+# a = set() # empty set
+
+# my_set = {1, 1, 1, 2, 3, 4, "banana"}
+# print(my_set)
+
+# my_set.add("sagar")
+# print(my_set)
+
+# my_set.update([123,"apple"])
+# print(my_set)
+
+# my_set.remove(123)
+# my_set.discard(123)
+# print(my_set)
+
+A = {1, 2, 3}
+B = {3, 4, 5}
+print(A | B)
+print(A & B)
+print(A - B)
+print(B - A)
+print(A ^ B)
+
+data = frozenset([1, 2, 3]) # immutable
+
+s = {"a", "b", "c"}
+print("a" in s)   # True
+print("d" not in s)   # True
--- day9.py
+++ day9.py
@@ -1,20 +1,77 @@
 # function : builtin function, user_defined function, arguments, return type
+# positional arguments, keyword arguments
 
-count = 0
+# count = 0
+# def test():
+#     global count
+#     count += 1
+#     print("This is test function ",count)
+
+# test()
+# test()
+# test()
+
+# print("----------------")
+# def add(a,b,c):
+#     sum = a+b+c
+#     return(sum)
+
+# a,b,c = 3,5,2
+# sum1 = print(f"The sum of {a,b,c} is :", add(a,b,c))
+# sum2 = print("The sum is:", add(4,6,5))
+
+
+def user_info(fname, lname):
+    return f"my name is: {fname} {lname}"
+
+
+# positional args
+result = user_info("sharma", "sagar")
+print(result)
+
+# keyword args
+result = user_info(lname="sharma", fname="sagar")
+print(result)
+
+# def test():
+#     # TODO: make this program runnable
+#     # FIXME: fix error here
+#     pass
+# def test():...
+
+print("------------------------")
+
+
+def add():
+    return (1, 2)
+
+
+print(add())
+
+
 def test():
-    global count
-    count += 1
-    print("This is test function ",count)
+    return 1, 2
 
-test()
-test()
-test()
 
-print("----------------")
-def add(a,b,c):
-    sum = a+b+c
-    return(sum)
+a, b = test()
+print(a, b)
 
-a,b,c = 3,5,2
-sum1 = print(f"The sum of {a,b,c} is :", add(a,b,c))
-sum2 = print("The sum is:", add(4,6,5))
+print("------------------------")
+
+
+def add_num(data):
+    sum = 0
+    for i in data:
+        sum = sum + i
+    return sum
+
+
+result = add_num([1, 2, 3, 4, 5])
+print(result)
+
+print("------------------------")
+def area(r,pie=3.14):
+    return pie*r*r
+
+print(area(5))
+print(area(5,4))
